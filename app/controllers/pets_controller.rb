72cc86da74408b315e0dd29dc4a89fa931565db0
Raveen K Sharma <raveen08@gmail.com>
2020-10-13 19:00:44 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -37,15 +37,13 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do
-    if !params[:pet].keys.include?("owner_id")
-    params[:pet]["owner_id"] = []
-    end
-    #######
+    #binding.pry
+
     @pet = Pet.find(params[:id])
     @pet.update(params[:pet])
     
     if !params["owner"]["name"].empty?
-      @pet.owner << Owner.create(name: params["owner"]["name"])
+      @pet.owner = Owner.create(name: params["owner"]["name"])
     end
     
     @pet.save
